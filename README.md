9c253d3c55555c77b5709ac033c066db0bdcee27
Peter Beshai <peter.beshai@gmail.com>
2016-06-14 12:31:40 -0400

    Make cell data and renderer use additional parameters

--- README.md
+++ README.md
@@ -52,15 +52,15 @@ Here `Renderable` means anything React can render (e.g., React.Component, String
 | id | String | Yes | The id of the column. Typically corresponds to a key in the rowData object. |
 | className | String |  | The class name to be applied to both `<td>` and `<th>` |
 | header | Renderable |  | What is rendered in the column header. If not provided, uses the columnId. |
-| highlight | Function |  | `function (cellData, columnSummary, columnDefinition, rowData, rowNumber, tableData, columnId, columnsDefinition)`<br>A function that returns a boolean indicating whether or not to highlight the cell. |
-| renderer | Function |  | `function (cellData, columnDefinition, rowData, rowNumber, tableData, columnId, columnsDefinition)`<br>The function that renders the value in the table. Can return anything React can render. |
+| highlight | Function |  | `function (cellData, columnSummary, columnDefinition, rowData, rowNumber, tableData, columnsDefinition)`<br>A function that returns a boolean indicating whether or not to highlight the cell. |
+| renderer | Function |  | `function (cellData, columnDefinition, rowData, rowNumber, tableData, columnsDefinition)`<br>The function that renders the value in the table. Can return anything React can render. |
 | rendererOptions | Object |  | Object of options that can be read by the renderer |
-| simpleRenderer | Function |  | `function (cellData, columnDefinition, rowData, rowNumber, tableData, columnId, columnsDefinition)`<br>The function that render the cell's value in a simpler format. Must return a String or Number. |
+| simpleRenderer | Function |  | `function (cellData, columnDefinition, rowData, rowNumber, tableData, columnsDefinition)`<br>The function that render the cell's value in a simpler format. Must return a String or Number. |
 | sortValue | Function |  | `function (cellData, rowData)`<br>Function to use when sorting instead of `value`. |
 | tdClassName | String |  | The class name to be applied to `<td>` only |
 | thClassName | String |  | The class name to be applied to `<th>` only |
 | type | String |  | The `DataType` of the column - number, string, etc |
-| value | String, Function |  | `function (rowData, rowNumber, tableData, columnId, columnsDefinition)`<br>Function to produce cellData's value. If a String, reads that as a key into the rowData object. If not provided, columnId is used as a key into the rowData object. |
+| value | String, Function |  | `function (rowData, rowNumber, tableData, columnsDefinition)`<br>Function to produce cellData's value. If a String, reads that as a key into the rowData object. If not provided, columnId is used as a key into the rowData object. |
 | width | String, Number |  | The value to set for the style `width` property on the column. |
 
 
--- src/TacoTableCell.js
+++ src/TacoTableCell.js
@@ -12,6 +12,9 @@ const propTypes = {
   /* The data to render in this row */
   rowData: React.PropTypes.object.isRequired,
 
+  /* The row number in the table */
+  rowNumber: React.PropTypes.number,
+
   /* The table data */
   tableData: React.PropTypes.array,
 };
@@ -20,9 +23,60 @@ const defaultProps = {
 
 };
 
-function getCellData(column, rowData) {
-  return column.value(rowData);
+/**
+ * Gets the value of a cell given the row data. If column.value is
+ * a function, it gets called, otherwise it is interpreted as a
+ * key to rowData. If column.value is not defined, column.id is
+ * used as a key to rowData.
+ *
+ * @param {Object} column The column definition
+ * @param {Object} rowData The data for the row
+ * @param {Number} rowNumber The number of the row
+ * @param {Array} tableData The array of data for the whole table
+ * @param {Array} columns The column definitions for the whole table
+ * @return {Any} The value for this cell
+ */
+function getCellData(column, rowData, rowNumber, tableData, columns) {
+  const { value, id } = column;
+  // call value as a function
+  if (typeof value === 'function') {
+    return value(rowData, rowNumber, tableData, columns);
+
+  // interpret value as a key
+  } else if (value != null) {
+    return rowData[value];
+  }
+
+  // otherwise, use the ID as a key
+  return rowData[id];
 }
+
+/**
+ * Renders a cell's contents based on the renderer function. If no
+ * renderer is provided, it just returns the raw cell data. In such
+ * cases, the user should take care that cellData can be rendered
+ * directly.
+ *
+ * @param {Any} cellData The data for the cell
+ * @param {Object} column The column definition
+ * @param {Object} rowData The data for the row
+ * @param {Number} rowNumber The number of the row
+ * @param {Array} tableData The array of data for the whole table
+ * @param {Array} columns The column definitions for the whole table
+ * @return {Renderable} The contents of the cell
+ */
+function renderCell(cellData, column, rowData, rowNumber, tableData, columns) {
+  const { renderer } = column;
+
+  // if renderer is provided, call it
+  if (renderer != null) {
+    return renderer(cellData, column, rowData, rowNumber, tableData, columns);
+  }
+
+  // otherwise, render the raw cell data
+  return cellData;
+}
+
 
 class TacoTableCell extends React.Component {
   shouldComponentUpdate(nextProps, nextState) {
@@ -30,14 +84,14 @@ class TacoTableCell extends React.Component {
   }
 
   render() {
-    const { column, rowData } = this.props;
-    const { className, tdClassName, renderer } = column;
+    const { column, rowData, rowNumber, tableData, columns } = this.props;
+    const { className, tdClassName } = column;
 
-    const cellData = getCellData(column, rowData);
-
+    const cellData = getCellData(column, rowData, rowNumber, tableData, columns);
+    const rendered = renderCell(cellData, column, rowData, rowNumber, tableData, columns);
     return (
       <td className={classNames(className, tdClassName)}>
-        {renderer(cellData)}
+        {rendered}
       </td>
     );
   }
