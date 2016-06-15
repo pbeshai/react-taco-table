a713c8bb7f79dc5812789e4906bece9b7fb4f9c6
Peter Beshai <peter.beshai@gmail.com>
2016-06-15 18:01:46 -0400

    Add sort direction docs

--- src/SortDirection.js
+++ src/SortDirection.js
@@ -1,5 +1,14 @@
+/** @module SortDirection */
+
+/**
+ * Sort direction enum for TacoTable columns
+ * @enum {Boolean}
+ */
 const SortDirection = {
+  /** true */
   Ascending: true,
+
+  /** false */
   Descending: false,
 };
 
