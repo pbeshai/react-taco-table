3f58bd0ceef41f9fc41158125ae25a136b09d327
Peter Beshai <peter.beshai@gmail.com>
2016-06-15 18:05:39 -0400

    Improve summarizer docs

--- src/Summarizers.js
+++ src/Summarizers.js
@@ -1,9 +1,22 @@
-/** @module Summarizers */
+/**
+ *
+ * Summarizers are functions that derive some properties over the data for
+ * an entire column.
+ *
+ * They all have the signature: `summarizer(column, tableData, columns)` where
+ * column is the column definition, tableData is the data for the entire table,
+ * and columns is the configuration for all the columns in the table.
+ *
+ * They should return an object with values derived from the column's worth of
+ * data.
+ *
+ * @module Summarizers
+ */
 
 import * as Utils from './Utils';
 
 /**
- * Computes the minimum and maximum values in a column
+ * Computes the minimum and maximum values in a column as `min` and `max`.
  *
  * @param {Object} column The column definition
  * @param {Array} tableData the data for the whole table
