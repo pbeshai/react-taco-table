583274035d8eef0ba2e73345c33a4025968ad5fc
Irene Ros <imirene@gmail.com>
2016-06-15 12:53:01 -0400

    added a few formatte tests, eslint rules and eslint fixes

--- tests/Formatters.js
+++ tests/Formatters.js
@@ -1,10 +1,8 @@
 import * as Formatters from '../src/Formatters';
 
 describe('Formatters', function () {
-
   const identity = d => d;
   describe('safeFormat', function () {
-    
     it('should return null when passed a null', function () {
       assert.equal(Formatters.safeFormat(null, identity), null);
     });
@@ -15,6 +13,29 @@ describe('Formatters', function () {
 
     it('should return value when passed a value that is safe', function () {
       assert.equal(Formatters.safeFormat(5, identity), 5);
+    });
+  });
+
+  describe('zeroAsNull', function () {
+    const zeroAsNullInstance = Formatters.zeroAsNull(identity);
+    it('should return null when passed a zero', function () {
+      assert.equal(zeroAsNullInstance(0), null);
+    });
+
+    it('should return a normal value', function () {
+      assert.equal(zeroAsNullInstance(1), 1);
+    });
+  });
+
+  describe('makePlusMinus', function () {
+    const makePlusMinusInstance = Formatters.makePlusMinus(identity);
+
+    it('should add a + to a positive value', function () {
+      assert.equal(makePlusMinusInstance(3), '+3');
+    });
+
+    it('should keep a - for a negative value', function () {
+      assert.equal(makePlusMinusInstance(-2), '-2');
     });
   });
 });
