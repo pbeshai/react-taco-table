f360d435fdb7704967e7c726c79180086041e735
Peter Beshai <peter.beshai@gmail.com>
2016-06-15 18:13:09 -0400

    Improve TdClassNames docs

--- src/TdClassNames.js
+++ src/TdClassNames.js
@@ -1,22 +1,42 @@
-/** @module TdClassNames */
+/**
+ * TdClassNames is a collection of utility functions that can be used
+ * in column definitions as the `tdClassName` attribute.
+ *
+ * These functions typically combine cell data and summary information
+ * to produce a meaningful class name.
+ *
+ * They can take the following arguments:
+ *  - `cellData` {Object} The data for the cell
+ *  - `columnSummary` {Object} The summary for the column
+ *  - `column` {Object} The definition for the column
+ *  - `rowData` {Object} The data for the row
+ *  - `highlightedColumn` {Boolean} true if this column is highlighted
+ *  - `highlightedRow` {Boolean} true if this row is highlighted
+ *  - `rowNumber` {Number} The row number
+ *  - `tableData` {Array} the data for the whole table
+ *  - `columns` {Array} the definitions for all columns
+ *
+ * @module TdClassNames
+ */
 
 import * as Utils from './Utils';
 
 /**
- * Adds highlight-min-max and one of highlight-min, highlight-max to the
+ * Adds `highlight-min-max` and one of `highlight-min`, `highlight-max` to the
  * cells that match min and max in the summary
  *
+ * @param {Any} cellData the data for the cell
+ * @param {Object} columnSummary The column summary
  * @param {Object} column The column definition
- * @param {Array} tableData the data for the whole table
- * @param {Array} columns The definitions of columns for the whole table
+ * @param {Array} rowData the data for the row
  *
  * @return {String} classnames
  */
-export function minMaxClassName(cellData, summary, column, rowData) {
+export function minMaxClassName(cellData, columnSummary, column, rowData) {
   const sortValue = Utils.getSortValueFromCellData(cellData, column, rowData);
-  if (sortValue === summary.min) {
+  if (sortValue === columnSummary.min) {
     return 'highlight-min-max highlight-min';
-  } else if (sortValue === summary.max) {
+  } else if (sortValue === columnSummary.max) {
     return 'highlight-min-max highlight-max';
   }
 
@@ -24,17 +44,18 @@ export function minMaxClassName(cellData, summary, column, rowData) {
 }
 
 /**
- * Adds highlight-min to the cells that match min in the summary.
+ * Adds `highlight-min` to the cells that match min in the summary.
  *
+ * @param {Any} cellData the data for the cell
+ * @param {Object} columnSummary The column summary
  * @param {Object} column The column definition
- * @param {Array} tableData the data for the whole table
- * @param {Array} columns The definitions of columns for the whole table
+ * @param {Array} rowData the data for the row
  *
  * @return {String} classnames
  */
-export function minClassName(cellData, summary, column, rowData) {
+export function minClassName(cellData, columnSummary, column, rowData) {
   const sortValue = Utils.getSortValueFromCellData(cellData, column, rowData);
-  if (sortValue === summary.min) {
+  if (sortValue === columnSummary.min) {
     return 'highlight-min';
   }
 
@@ -42,17 +63,18 @@ export function minClassName(cellData, summary, column, rowData) {
 }
 
 /**
- * Adds highlight-max to the cells that match max in the summary.
+ * Adds `highlight-max` to the cells that match max in the summary.
  *
+ * @param {Any} cellData the data for the cell
+ * @param {Object} columnSummary The column summary
  * @param {Object} column The column definition
- * @param {Array} tableData the data for the whole table
- * @param {Array} columns The definitions of columns for the whole table
+ * @param {Array} rowData the data for the row
  *
  * @return {String} classnames
  */
-export function maxClassName(cellData, summary, column, rowData) {
+export function maxClassName(cellData, columnSummary, column, rowData) {
   const sortValue = Utils.getSortValueFromCellData(cellData, column, rowData);
-  if (sortValue === summary.max) {
+  if (sortValue === columnSummary.max) {
     return 'highlight-max';
   }
 
