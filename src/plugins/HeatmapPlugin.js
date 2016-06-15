f4f04f3910831853468be0b723dcc0ebdc05a21d
Peter Beshai <peter.beshai@gmail.com>
2016-06-15 17:02:02 -0400

    Improve heat map docs

--- src/plugins/HeatmapPlugin.js
+++ src/plugins/HeatmapPlugin.js
@@ -1,8 +1,18 @@
-/** @module HeatmapPlugin
+/**
  * Adds heatmap coloring to cells. By default colors based on min and max in the data and
- * the inferno color scheme via d3-scale.
+ * the inferno color scheme via `d3-scale`.
  *
- * Plugin options found in `plugins.heatmap` since the plugin has id `heatmap`
+ * This plugin provides the following fields:
+ * - id: `heatmap`
+ * - tdClassName: `has-heatmap`
+ * - summarize: minMaxSummarizer
+ * - tdStyle
+ * - columnTest
+ * - ColorSchemes
+ *
+ * ## Plugin Options
+ *
+ * Plugin options found in `column.plugins.heatmap` since the plugin has id `heatmap`.
  *
  * - **domain** {Array} the domain to use for the color scale, if not provided, uses summary min and max
  * - **backgroundScale** {Function} the scale to use for the background-color
@@ -10,27 +20,29 @@
  * - **colorShift** {Number} if provided, shifts the background color to create the foreground color.
  *    It is a number between 0 and 1 describing how far to shift.
  * - **colorScheme** {String} if provided, specifies which d3-scale special scale to use. Options
- *    are: Viridis, Inferno, Magma, Plasma, Warm, Cool, Rainbow
+ *    are: Viridis, Inferno, Magma, Plasma, Warm, Cool, Rainbow, CubehelixDefault
+ *
+ * @module plugins/HeatmapPlugin
  */
 import * as Utils from '../Utils';
 import * as Summarizers from '../Summarizers';
 import DataType from '../DataType';
 import * as d3 from 'd3-scale';
-import keyMirror from 'keymirror';
 
 /**
  * Special color schemes available via d3-scale `interpolate${scheme}(t)` functions.
+ * @enum {String}
  */
-const ColorSchemes = keyMirror({
-  Viridis: null,
-  Inferno: null,
-  Magma: null,
-  Plasma: null,
-  Warm: null,
-  Cool: null,
-  Rainbow: null,
-  CubehelixDefault: null,
-});
+const ColorSchemes = {
+  Viridis: 'Viridis',
+  Inferno: 'Inferno',
+  Magma: 'Magma',
+  Plasma: 'Plasma',
+  Warm: 'Warm',
+  Cool: 'Cool',
+  Rainbow: 'Rainbow',
+  CubehelixDefault: 'CubehelixDefault',
+};
 
 /**
  * Compute the style for the td elements by setting the background and color
