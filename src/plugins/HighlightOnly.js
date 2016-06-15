11bd81dda54796dc7de9a8be79608e8c79e837af
Peter Beshai <peter.beshai@gmail.com>
2016-06-15 17:46:45 -0400

    Add in documentation to highlight only

--- src/plugins/HighlightOnly.js
+++ src/plugins/HighlightOnly.js
@@ -1,11 +1,30 @@
-/** @module HighlightOnly
+/**
  * Wraps a plugin so that it only activates its tdStyle and tdClassName
  * when the cell is highlighted, unless a column is configured otherwise.
+ *
+ * ## Options
+ *
+ * Plugins can configure explicit overrides using the `highlight` property
+ * within their column-based configuration. `highlight` can be set to `'always'`
+ * to ensure the plugin is run for that column regardless of highlight.
+ *
+ * For example,
+ * `column.plugins.heatmap.highlight = 'always'` will disable HighlightOnly
+ *  for the column if HighlightOnly is applied to a plugin with id `heatmap`.
+ *
+ * - onRowHighlight {Boolean} set to true means apply the plugin on row highlight
+ * - onColumnHighlight {Boolean} set to true means apply the plugin on column highlight
+ *
+ * @example
+ * <TacoTable plugins={[HighlightOnly(HeatmapPlugin, { onRowHighlight: true, onColumnHighlight: true })]} ... />
+ *
+ * @module plugins/HighlightOnly
  */
 
 /**
- * onRowHighlight set to true means apply the plugin on row highlight
- * onColumnHighlight set to true means apply the plugin on column highlight
+ * @param {Object} Plugin The Plugin definition (e.g. `HeatmapPlugin`)
+ * @param {Object} options The options for HighlightOnly
+ * @return {Object} A new plugin that wraps `Plugin`'s tdStyle and tdClassName
  */
 function HighlightOnly(Plugin, options = { onRowHighlight: true, onColumnHighlight: true }) {
   function highlightOnlyHelper(pluginFunc) {
