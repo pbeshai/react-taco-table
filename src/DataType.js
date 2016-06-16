2ddf1abe739576e30e900824a347b10219c9d1c6
Peter Beshai <peter.beshai@gmail.com>
2016-06-16 11:33:00 -0400

    Add in docs about number vs number ordinal

--- src/DataType.js
+++ src/DataType.js
@@ -5,8 +5,12 @@
  * @enum {String}
  */
 const DataType = {
+  /** Should be used for numeric types */
   Number: 'Number',
+
+  /** Should be used for numeric types that can also work as categories (e.g., year) */
   NumberOrdinal: 'NumberOrdinal',
+
   String: 'String',
   Boolean: 'Boolean',
   Date: 'Date',
