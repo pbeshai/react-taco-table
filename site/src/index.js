3d125df000976b49a054a463de9dac4957546b35
Peter Beshai <peter.beshai@gmail.com>
2016-06-20 10:31:47 -0400

    Remove unnecessary Main component for index page

--- site/src/index.js
+++ site/src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import Main from './components/Main';
+import SimpleExample from './components/SimpleExample';
 
 import './site.scss';
 import 'react-taco-table/style/taco-table.scss';
 
+// add in performance tooling in dev
+if (process.env.NODE_ENV !== 'production') {
+  window.Perf = require('react-addons-perf');
+}
 
-
-render(<Main />, document.getElementById('react-root'));
+render(<SimpleExample />, document.getElementById('react-root'));
