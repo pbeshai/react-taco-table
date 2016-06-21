ffa1592ea6827f48236a30bc618fb307b326fcdc
Peter Beshai <peter.beshai@gmail.com>
2016-06-21 12:46:40 -0400

    Improve code example to show preview of code

--- site/src/components/CodeExample.js
+++ site/src/components/CodeExample.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import classNames from 'classnames';
+
 import './code-example.scss';
 
 const propTypes = {
   language: React.PropTypes.string,
   code: React.PropTypes.string,
+  previewStart: React.PropTypes.number,
+  previewLength: React.PropTypes.number,
 };
 
-const defaultPropTypes = {
+const defaultProps = {
   language: 'javascript',
+  previewStart: 8,
+  previewLength: 8,
 };
 
 class CodeExample extends React.Component {
@@ -43,34 +49,69 @@ class CodeExample extends React.Component {
 
   // assumes hljs is in the window
   syntaxHighlight() {
-    if (this.refs.pre) {
-      window.hljs.highlightBlock(this.refs.pre);
+    if (this.refs.prePreview) {
+      window.hljs.highlightBlock(this.refs.prePreview);
+    }
+    if (this.refs.preFull) {
+      window.hljs.highlightBlock(this.refs.preFull);
     }
   }
 
-  render() {
-    const { language, code } = this.props;
-    const { expanded } = this.state;
+  renderCollapsed() {
+    const { language, code, previewStart, previewLength } = this.props;
 
-    if (!expanded) {
-      return (
-        <div className="code-example collapsed" onClick={this.handleExpand}>
-          Show Code
-        </div>
-      );
+    const codeLines = code.split('\n');
+    let previewCode;
+
+    if (codeLines.length > previewLength) {
+      previewCode = codeLines
+        .slice(previewStart, previewStart + previewLength)
+        .concat(['...'])
+        .join('\n');
+    } else {
+      previewCode = code;
     }
 
     return (
-      <div className="code-example expanded">
-        <h4 onClick={this.handleCollapse}>Code</h4>
-        <pre ref="pre"><code className={language}>{code}</code></pre>
+      <div className="preview-block">
+        <pre className="preview-code" ref="prePreview">
+          <code className={language}>{previewCode}</code>
+        </pre>
+        <div className="code-toggle-control" onClick={this.handleExpand}>Show All Code</div>
+      </div>
+    );
+  }
+
+  renderExpanded() {
+    const { language, code } = this.props;
+    return (
+      <div className="expanded-block">
+        <pre className="expanded-code" ref="preFull">
+          <code className={language}>{code}</code>
+        </pre>
+        <div className="code-toggle-control" onClick={this.handleCollapse}>Hide Code</div>
+      </div>
+    );
+  }
+
+  render() {
+    const { expanded } = this.state;
+
+    // note need to render both to get highlighting to work with hljs, but only show one
+    // (both = collapsed and expanded)
+
+    return (
+      <div className={classNames('code-example', { expanded, collapsed: !expanded })}>
+        <h4>Code</h4>
+        {this.renderCollapsed()}
+        {this.renderExpanded()}
       </div>
     );
   }
 }
 
 CodeExample.propTypes = propTypes;
-CodeExample.defaultPropTypes = defaultPropTypes;
+CodeExample.defaultProps = defaultProps;
 
 
 export default CodeExample;
