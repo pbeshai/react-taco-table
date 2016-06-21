cb58cb7316c9210d55f00cafe6243e7eed9580d9
Peter Beshai <peter.beshai@gmail.com>
2016-06-21 16:14:15 -0400

    Add in a frequency summarizer

--- src/Summarizers.js
+++ src/Summarizers.js
@@ -17,12 +17,13 @@ import * as Utils from './Utils';
 
 /**
  * Computes the minimum and maximum values in a column as `min` and `max`.
+ * Does the computation based on the sortValue.
  *
  * @param {Object} column The column definition
  * @param {Object[]} tableData the data for the whole table
  * @param {Object[]} columns The definitions of columns for the whole table
  *
- * @return {Object} The minimum and maximum values as {min, max}
+ * @return {Object} The minimum and maximum values as `{ min, max }`
  */
 export function minMaxSummarizer(column, tableData, columns) {
   return tableData.reduce((minMax, rowData, rowNumber) => {
@@ -41,3 +42,34 @@ export function minMaxSummarizer(column, tableData, columns) {
   }, {});
 }
 
+/**
+ * Computes the frequency for each value in the dataset and computes the most frequent.
+ * If there is a tie for most frequent, it returns just the first encountered value.
+ * Does the summary based on the cellData value.
+ *
+ * @param {Object} column The column definition
+ * @param {Object[]} tableData the data for the whole table
+ * @param {Object[]} columns The definitions of columns for the whole table
+ *
+ * @return {Object} The most frequently occuring value `{ counts, mostFrequent }`
+ */
+export function frequencySummarizer(column, tableData, columns) {
+  let mostFrequent;
+
+  const counts = tableData.reduce((counts, rowData, rowNumber) => {
+    const cellData = Utils.getCellData(column, rowData, rowNumber, tableData, columns);
+    if (!counts[cellData]) {
+      counts[cellData] = 1;
+    } else {
+      counts[cellData] += 1;
+    }
+
+    if (mostFrequent == null || (counts[cellData] > counts[mostFrequent])) {
+      mostFrequent = cellData;
+    }
+
+    return counts;
+  }, {});
+
+  return { counts, mostFrequent };
+}
